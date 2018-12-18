9b7397d5db1c6cb8c3b1dea137884c42cb485159
achintyapataskar <apataskar780@gmail.com>
2018-12-18 16:26:13 +0530

    Fixed coding style in CharacterType question

--- PE1/CharacterType.java
+++ PE1/CharacterType.java
@@ -2,22 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class CharacterType {
-    public static void main(String args[]) {
-        char c;
-        Scanner in = new Scanner(System.in);
-        c = in.next().charAt(0);
-        if(c >= 'a' && c <= 'z') {
+    public static void type(char input) {
+        if(input >= 'a' && input <= 'z') {
             System.out.println("Lower Case Character");
             System.exit(0);
         }
-        if(c >= 'A' && c <= 'Z') {
+        if(input >= 'A' && input <= 'Z') {
             System.out.println("Upper Case Character");
             System.exit(0);
         }
-        if(c >= '0' && c <= '9') {
+        if(input >= '0' && input <= '9') {
             System.out.println("Digit");
             System.exit(0);
         }
         System.out.println("Special Symbol");
+    }
+    public static void main(String args[]) {
+        char input;
+        Scanner in = new Scanner(System.in);
+        input = in.next().charAt(0);
+        type(input);
     }
 }
